15c32554a2cbf7c2fdfa10e954605d52a71022fc
Peacecake <sebastian.peiser@stud.uni-regensburg.de>
2017-07-15 16:03:11 +0200

    implemented connection to wiimote

--- Code/iplanpy.py
+++ Code/iplanpy.py
@@ -1,16 +1,46 @@
+# !/usr/bin/env python3
+# coding: utf-8
+# -*- coding: utf-8 -*-
+
 import sys
 from PyQt5 import uic, QtWidgets, QtCore, QtGui
+import wiimote
 
 
 class IPlanPy(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.wiimote = None
         self.init_ui()
 
     def init_ui(self):
-        print("load_ui")
         self.ui = uic.loadUi("iplanpy.ui", self)
+        self.ui.btn_connect_wiimote.clicked.connect(self.toggle_wiimote_connection)
         self.show()
+
+    def toggle_wiimote_connection(self):
+        if self.wiimote is not None:
+            self.disconnect_wiimote()
+            return
+        else:
+            self.ui.lbl_wiimote_address.setText("Connecting...")
+            results = wiimote.find()
+            if len(results) > 0:
+                addr, name = results[0]
+                self.wiimote = wiimote.connect(addr, name)
+                if self.wiimote is None:
+                    self.ui.btn_connect_wiimote.setText("Connect")
+                else:
+                    self.ui.btn_connect_wiimote.setText("Disconnect")
+                    self.ui.lbl_wiimote_address.setText("Connected to " + addr)
+            else:
+                self.ui.btn_connect_wiimote.setText("Connect")
+
+    def disconnect_wiimote(self):
+        self.wiimote.disconnect()
+        self.wiimote = None
+        self.ui.btn_connect_wiimote.setText("Connect")
+        self.ui.lbl_wiimote_address.setText("Not connected")
 
 
 if __name__ == '__main__':
