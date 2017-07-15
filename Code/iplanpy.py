8349cbaac183549ba40f833b3278e824448e8191
Peacecake <sebastian.peiser@stud.uni-regensburg.de>
2017-07-15 20:40:04 +0200

    added callbacks for buttons and ir

--- Code/iplanpy.py
+++ Code/iplanpy.py
@@ -22,25 +22,42 @@ class IPlanPy(QtWidgets.QWidget):
         if self.wiimote is not None:
             self.disconnect_wiimote()
             return
-        else:
-            self.ui.lbl_wiimote_address.setText("Connecting...")
-            results = wiimote.find()
-            if len(results) > 0:
-                addr, name = results[0]
-                self.wiimote = wiimote.connect(addr, name)
-                if self.wiimote is None:
-                    self.ui.btn_connect_wiimote.setText("Connect")
-                else:
-                    self.ui.btn_connect_wiimote.setText("Disconnect")
-                    self.ui.lbl_wiimote_address.setText("Connected to " + addr)
-            else:
+        self.connect_wiimote()
+
+    def connect_wiimote(self):
+        self.ui.btn_connect_wiimote.setText("Connecting...")
+        results = wiimote.find()
+        if len(results) > 0:
+            addr, name = results[0]
+            self.wiimote = wiimote.connect(addr, name)
+            if self.wiimote is None:
                 self.ui.btn_connect_wiimote.setText("Connect")
+            else:
+                self.ui.btn_connect_wiimote.setText("Disconnect")
+                self.ui.lbl_wiimote_address.setText("Connected to " + addr)
+                self.wiimote.buttons.register_callback(self.on_wiimote_button)
+                self.wiimote.ir.register_callback(self.on_wiimote_ir)
+                self.wiimote.rumble()
+        else:
+            self.ui.btn_connect_wiimote.setText("Connect")
 
     def disconnect_wiimote(self):
         self.wiimote.disconnect()
         self.wiimote = None
         self.ui.btn_connect_wiimote.setText("Connect")
         self.ui.lbl_wiimote_address.setText("Not connected")
+
+    def on_wiimote_button(self, event):
+        if len(event) is not 0:
+            button, is_pressed = event[0]
+            if is_pressed:
+                print("Button " + button + " is pressed")
+            else:
+                print("Button " + button + " is released")
+
+    def on_wiimote_ir(self, event):
+        if len(event) is not 0:
+            print(event)
 
 
 if __name__ == '__main__':
