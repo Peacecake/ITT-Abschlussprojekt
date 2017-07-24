f4d9c185126f13d4ee04c18a5553be786aae985a
Julesn <Julia.Sageder@stud.uni-regensburg.de>
2017-07-24 18:16:11 +0100

    change color of card with wii/button B

--- Code/iplanpy.py
+++ Code/iplanpy.py
@@ -202,6 +202,18 @@ class IPlanPy(QtWidgets.QWidget):
             button, is_pressed = event[0]
             if is_pressed:
                 print("Button " + button + " is pressed")
+                if button == 'A' and self.ui.lbl_new_card.underMouse():
+                    print('New Card with Wii')
+                    # Doesnt work because: QObject::setParent: Cannot set parent, new parent is in a different thread
+
+                    # card = Card(self)
+                    # new_y = self.ui.fr_control_container.size().height()
+                    # card.setGeometry(0, new_y, card.size().width(), card.size().height())
+                    # self.all_cards.append(card)
+                if button == 'B' and (self.get_card_under_mouse() is not None):
+                    print('Color changed with Wii')
+                    print(str(self.get_card_under_mouse()))
+                    self.changeColor(self.get_card_under_mouse())
             else:
                 print("Button " + button + " is released")
 
@@ -216,17 +228,19 @@ class IPlanPy(QtWidgets.QWidget):
     def on_wiimote_accelerometer(self, event):
         self.classifier.add_accelerometer_data(event[0], event[1], event[2])
 
+    def changeColor(self, curr_card):
+        style = str(curr_card.styleSheet())
+        print(style)
+        if 'rgb(85, 170, 255)' in style:
+            curr_card.setStyleSheet(self.bg_colors[1])
+        elif 'red' in style:
+            curr_card.setStyleSheet(self.bg_colors[2])
+        else:
+            curr_card.setStyleSheet(self.bg_colors[0])
+
     def mousePressEvent(self, event):
         if event.button() == QtCore.Qt.RightButton:
-            curr_card = self.get_card_under_mouse()
-            style = str(curr_card.styleSheet())
-            print(style)
-            if 'rgb(85, 170, 255)' in style:
-                curr_card.setStyleSheet(self.bg_colors[1])
-            elif 'red' in style:
-                curr_card.setStyleSheet(self.bg_colors[2])
-            else:
-                curr_card.setStyleSheet(self.bg_colors[0])
+            self.changeColor(self.get_card_under_mouse())
 
         self.__mousePressPos = None
         self.__mouseMovePos = None
